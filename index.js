f31a61d61648e014519de527480b5e5be536460e
mosn85 <43811713+mosn85@users.noreply.github.com>
2020-02-17 15:51:48 +0000

    Added index.js file

---
+++ index.js
@@ -0,0 +1,9 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Welcome Node.js');
+}).listen(3001, "localhost");
+exports.handler =  async function(event, context) {
+        console.log('Server running at http://localhost:3001/')
+        return context.logStreamName
+}
